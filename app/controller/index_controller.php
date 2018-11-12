1d9c68511da227c77fc3345c6b3e2fefadb54f74
Andree Ochoa <andlody@hotmail.com>
2018-11-12 16:28:27 -0500

    s

--- app/controller/index_controller.php
+++ app/controller/index_controller.php
@@ -48,7 +48,7 @@ class index_controller extends Controller
 		if($USER->id == 0){
 			header("Location: /");
 		}else{
-			$this->set('full_name',$USER->firstname.' '.$USER->lastname);echo 'rpueba';return;
+			$this->set('full_name',$USER->firstname.' '.$USER->lastname);
 			$obj = $this->model('Sedes');
 			$n = $obj->esJefe($USER->id);
 			if($n == 1 || $n == 11){
